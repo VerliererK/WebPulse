c5522e5ab9eac3cf351e3e62c73990233acd74ee
VerliererK <r03548014@ntu.edu.tw>
2025-04-26 22:22:43 +0800

    fix: ensure correct parsing of exchange rate values

--- src/job/jpy.ts
+++ src/job/jpy.ts
@@ -2,7 +2,7 @@ export default async (text: string) => {
   const res = await fetch('https://www.esunbank.com/api/client/ExchangeRate/LastRateInfo', { method: 'POST' });
   const data = await res.json() as { Rates: { Name: string, SellDecreaseRate: string, SBoardRate: string }[] };
   const jpyRate = data.Rates.find(rate => rate.Name === '日圓') || { SellDecreaseRate: '', SBoardRate: '' };
-  const rate = parseFloat(jpyRate.SellDecreaseRate || jpyRate.SBoardRate);
+  const rate = Number(jpyRate.SellDecreaseRate) || Number(jpyRate.SBoardRate);
 
   const update_text = `日円： ${rate}`;
   const needs_update = update_text !== text && rate * 10000 % 5 === 0;
