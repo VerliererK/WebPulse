dd109ad604e5d6df290782cdb0e27ed1dc5a047f
VerliererK <r03548014@ntu.edu.tw>
2025-05-02 14:49:02 +0800

    fix: enhance update condition for JPY exchange rate

--- src/job/jpy.ts
+++ src/job/jpy.ts
@@ -3,8 +3,9 @@ export default async (text: string) => {
   const data = await res.json() as { Rates: { Name: string, SellDecreaseRate: string, SBoardRate: string }[] };
   const jpyRate = data.Rates.find(rate => rate.Name === '日圓') || { SellDecreaseRate: '', SBoardRate: '' };
   const rate = Number(jpyRate.SellDecreaseRate) || Number(jpyRate.SBoardRate);
+  const lastRate = Number(text.split('： ')[1]) || 0;
 
   const update_text = `日円： ${rate}`;
-  const needs_update = update_text !== text && rate * 10000 % 5 === 0;
+  const needs_update = update_text !== text && (rate * 10000 % 5 === 0 || Math.abs(lastRate - rate) * 10000 >= 5);
   return { update_text, needs_update };
 }
